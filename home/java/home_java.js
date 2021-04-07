14689828f820a500671da94e26d956eba0fe77a8
eddyog <gon19005@byui.edu>
2021-04-06 23:37:33 -0600

    last update bm

--- home/java/home_java.js
+++ home/java/home_java.js
@@ -41,7 +41,7 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-var requestURL = 'https://raw.githubusercontent.com/eddyog/temple_suites.io/master/json/temples.json';
+var requestURL = 'https://eddyog.github.io/temples.json';
 var request = new XMLHttpRequest();
 
 request.open('GET', requestURL);
